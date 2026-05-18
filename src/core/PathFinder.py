8aa282b5bca9176238cbb14a7dfa83f1e1b66a61
Sara Fontes <sfontes94@gmail.com>
2026-05-18 15:56:36 +0100

    finished the MapParser and added the maps folder

--- src/core/PathFinder.py
+++ src/core/PathFinder.py
@@ -1,2 +1,8 @@
 class PathFinder:
-    pass
+    """
+    Uses get_connected_zones to see the near connections / neighbors
+    Uses BFS to find the shortest valid path
+    between the start and end zones.
+    Stores and returns discovered the path
+    To later be used on the Visualizer class
+    """
